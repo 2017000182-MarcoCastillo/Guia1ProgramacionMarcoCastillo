6a3215bcd67c99a7649f3c027c3f304cc6cc3e73
2017000182-MarcoCastillo <m.castillo.5cdb@gmail.com>
2026-04-18 12:13:27 -0600

    Add files via upload

---
+++ act2-inciso1-c#.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            Console.WriteLine("=== BUZÓN DE VOZ ===");
+            Console.WriteLine("1. Ventas");
+            Console.WriteLine("2. Soporte técnico");
+            Console.WriteLine("3. Atención al cliente");
+            Console.WriteLine("4. Dejar un mensaje");
+            Console.Write("Seleccione una opción (1-4): ");
+
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Ha seleccionado VENTAS. Un asesor le atenderá.");
+                    break;
+                case 2:
+                    Console.WriteLine("Ha seleccionado SOPORTE TÉCNICO. Espere en línea.");
+                    break;
+                case 3:
+                    Console.WriteLine("Ha seleccionado ATENCIÓN AL CLIENTE.");
+                    break;
+                case 4:
+                    Console.WriteLine("Por favor, deje su mensaje después del tono.");
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida. Intente nuevamente.");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ act2-leccion7-inciso2.psc
@@ -0,0 +1,24 @@
+Algoritmo sin_titulo
+	Definir nota Como Real
+	
+    Escribir "Ingrese la calificación:"
+    Leer nota
+	
+    Si nota < 60 Entonces
+        Escribir "Deficiente"
+    SiNo
+        Si nota < 70 Entonces
+            Escribir "Regular"
+        SiNo
+            Si nota < 80 Entonces
+                Escribir "Bien"
+            SiNo
+                Si nota < 90 Entonces
+                    Escribir "Muy bien"
+                SiNo
+                    Escribir "Excelente"
+                FinSi
+            FinSi
+        FinSi
+    FinSi
+FinAlgoritmo
