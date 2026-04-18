bde02bec7b0c025327b1768939a03f7979267e1b
2017000182-MarcoCastillo <m.castillo.5cdb@gmail.com>
2026-04-18 11:57:23 -0600

    Add files via upload

---
+++ ac1inciso2-c#.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+
+            Console.Write("Ingrese su edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("La persona es MAYOR de edad");
+            }
+            else
+            {
+                Console.WriteLine("La persona es MENOR de edad");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
