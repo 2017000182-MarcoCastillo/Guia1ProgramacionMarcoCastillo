0597871f8186dad5dcf155e36b845a507a66016b
2017000182-MarcoCastillo <m.castillo.5cdb@gmail.com>
2026-04-18 12:14:35 -0600

    Add files via upload

---
+++ act2-inciso2-c#.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota;
+
+        Console.Write("Ingrese la calificación: ");
+        nota = Convert.ToDouble(Console.ReadLine());
+
+        if (nota < 60)
+        {
+            Console.WriteLine("Deficiente");
+        }
+        else if (nota < 70)
+        {
+            Console.WriteLine("Regular");
+        }
+        else if (nota < 80)
+        {
+            Console.WriteLine("Bien");
+        }
+        else if (nota < 90)
+        {
+            Console.WriteLine("Muy bien");
+        }
+        else
+        {
+            Console.WriteLine("Excelente");
+        }
+
+        Console.ReadKey();
+    
+        }
+    }
+}
